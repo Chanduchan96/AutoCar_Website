4f4857d1697388942d3e79e6c26c75941ad89528
Chanduchan96 <116905272+Chanduchan96@users.noreply.github.com>
2023-02-19 18:11:57 +0530

    19-02-23

--- JS/script.js
+++ JS/script.js
@@ -38,3 +38,26 @@ window.onload=()=>{
         menu.classList.remove('fa-times');
         navbar.classList.remove('active');
     }
+
+    document.querySelector('.home').onmousemove =(e)=>
+    {
+        document.querySelectorAll('.home-parallax').forEach(elm=>{
+            let speed=elm.getAttribute('data-speed');
+
+            let x=(window.innerWidth - e.pageX*speed)/90;
+            let y=(window.innerHeight - e.pageY*speed)/90;
+
+            elm.style.transform=`translateX(${y}px) translateY(${x}px)`
+        });
+    }
+
+    document.querySelector('.home').onmouseleave =()=>
+    {
+        document.querySelectorAll('.home-parallax').forEach(elm=>{
+            let speed=elm.getAttribute('data-speed');
+
+          
+
+            elm.style.transform=`translateX(${0}px) translateY(${0}px)`
+        });
+    }
